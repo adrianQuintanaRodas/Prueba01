a6115052395121449785476731c5e6a9ce28728d
adrian quintana <adrianquintana2877@gmail.com>
2020-10-26 12:06:17 +0100

    estoy en Rama0

--- lunes/lunes/Form1.vb
+++ lunes/lunes/Form1.vb
@@ -2,5 +2,6 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         MsgBox("Hola Lunes")
         'comentario
+        'estoy en Rama0
     End Sub
 End Class
